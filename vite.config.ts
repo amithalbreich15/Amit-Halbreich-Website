cbf50f8758aa37d97cc4cddb1d97e2bea74cce3b
amithalbreich <amit.halbreich@mail.huji.ac.il>
2025-01-23 17:34:08 +0200

    Resolve merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/Amit-Halbreich-Website', // Change this if deploying to a subfolder, e.g., '/my-app/'
 });
