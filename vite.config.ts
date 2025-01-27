a1965e97805c9c2986ebe2cabc5afbc9cdc0ab47
amithalbreich <amit.halbreich@mail.huji.ac.il>
2025-01-27 15:51:03 +0200

    Changed vite.config.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/', // Change this if deploying to a subfolder, e.g., '/my-app/'
+  base: '/Amit-Halbreich-Website/', // This must match your GitHub repository name
 });
