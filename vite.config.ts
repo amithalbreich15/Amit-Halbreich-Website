85a08610d85856dfcf7eb78f97fce1d8a551be0e
amithalbreich15 <amithalbreich15@gmail.com>
2025-03-17 17:01:50 +0200

    Delete unnecessary files and change home route

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { ghPages } from 'vite-plugin-gh-pages';
+
+// export default defineConfig({
+//   plugins: [react(), ghPages()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   base: '/', // This must match your GitHub repository name
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
+// import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/Amit-Halbreich-Website/', // This must match your GitHub repository name
+  base: '/'
 });
+
